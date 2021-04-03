e4dbf19ac37729685e5bff9900477da554c621d9
mk5master <49418797+mk5master@users.noreply.github.com>
2021-04-02 20:20:01 -0500

    Add files via upload

---
+++ examples/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="styles.css" rel="stylesheet">
+    <title></title>
+  </head>
+  <body>
+
+  <?php echo "hola mundo manual sensillo de php ";   ?>
+
+<table border="5" class="definicion">
+
+  <tr>
+    <td>Que es php.</td>
+  </tr>
+  <tr>
+    <td>PHP, acrónimo de "PHP: HypertextPreprocessor", es un lenguaje de 'scripting' de propósito general y de código abierto que está especialmente pensado para el desarrolloweb y que puede ser embebido en páginas HTML. Su sintaxis recurrea C, Java y Perl, siendo así sencillo de aprender. El objetivo principal de estelenguaje es permitir a los desarrolladores web escribir dinámicay rápidamente páginas web generadas; aunque se puede hacer mucho más con PHP.</td>
+  </tr>
+</table>
+
+<br>
+
+<div class="phpbasico" >
+lo que esta en rojo es el hola mundo en php------
+<span style="color:rgb(228, 18, 18)"><?php echo " echo  (este es el echo hola mundo!! como estan)"; ?><span>
+
+</div>
+
+  </body>
+</html>
